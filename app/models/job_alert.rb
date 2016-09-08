e385f7519c2480ffcc048986e1a7a67cada1cbd0
Alex <alexduval71@gmail.com>
2016-09-08 15:09:28 +0200

    simple has_many model validation

--- app/models/job_alert.rb
+++ app/models/job_alert.rb
@@ -2,4 +2,5 @@ class JobAlert < ApplicationRecord
   belongs_to :job_categorie
   belongs_to :user
   belongs_to :ski_resort
+  has_many :connection_jobs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :job_alerts
+  has_many :connection_jobs
 end
