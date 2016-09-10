1fe77c05611b50bff23a7f43c36d31999aed409e
Alex <alexduval71@gmail.com>
2016-09-10 19:31:02 +0200

    structure seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 ##############  SKI  RESORT  ################
 puts "Destoy Ski Resort"
-SkiResort.destroy_all
+  SkiResort.destroy_all
 puts "Creation Ski Resort"
 skiresorts = ["Val d'Isère", "Méribel", "Val Thorens", "Tignes"]
 skiresorts.each do |skiresort|
@@ -13,7 +13,28 @@ puts "Creation Ski Resort COMPLETE & SUCCEED"
 
 ##############      USER   ###############################
 puts "Destoy "
-User.destroy_all
+  User.destroy_all
+puts "Creation "
+
+puts "Creation  COMPLETE & SUCCEED"
+
+
+##############      CONNECTION JOB   ###############################
+puts "Destoy "
+puts "Creation "
+
+puts "Creation  COMPLETE & SUCCEED"
+
+
+##############      JOB ALERT   ###############################
+puts "Destoy "
+puts "Creation "
+
+puts "Creation  COMPLETE & SUCCEED"
+
+
+##############      JOB CATEGORIE   ###############################
+puts "Destoy "
 puts "Creation "
 
 puts "Creation  COMPLETE & SUCCEED"
