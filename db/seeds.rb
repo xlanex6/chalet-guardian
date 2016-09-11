4f790298cfd6ba01a3d679e082a686f7c2c1a1ec
Alex <alexduval71@gmail.com>
2016-09-11 08:21:15 +0200

    complete seed with job CATEGORIE

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 ##############  SKI  RESORT  ################
 puts "Destoy Ski Resort"
   SkiResort.destroy_all
-puts "Creation Ski Resort"
-skiresorts = ["Val d'Isère", "Méribel", "Val Thorens", "Tignes"]
+puts "Creation Ski Resort..."
+skiresorts = ["Val d'Isère", "Méribel", "Courchevel 1550", "Courchevel 1650", "Courchevel 1850", "Chamonix"]
 skiresorts.each do |skiresort|
   SkiResort.create(
     name: skiresort
@@ -12,29 +12,38 @@ puts "Creation Ski Resort COMPLETE & SUCCEED"
 
 
 ##############      USER   ###############################
-puts "Destoy "
+puts "Destoy ALL User"
   User.destroy_all
-puts "Creation "
+puts "User Creation..."
 
 puts "Creation  COMPLETE & SUCCEED"
 
 
 ##############      CONNECTION JOB   ###############################
 puts "Destoy "
-puts "Creation "
+puts "Creation..."
 
 puts "Creation  COMPLETE & SUCCEED"
 
 
 ##############      JOB ALERT   ###############################
 puts "Destoy "
-puts "Creation "
+puts "Creation..."
 
 puts "Creation  COMPLETE & SUCCEED"
 
 
 ##############      JOB CATEGORIE   ###############################
-puts "Destoy "
-puts "Creation "
+puts "Destoy ALL Ski Resort"
+  JobCategorie.destroy_all
+puts "Ski Resort Creation..."
+jobscats = ["Administration", "Bar Staff", "Catering", "Chalet Staff", "Chef","Assistant Chef", "Childcare", "Customer Service", "Driving / Transport", "Facilities / Maintenance", "Head Chef", "Health / Beauty", "Housekeeping", "Instructors / Guides", "Management", "Resort Reps", "Retail" ]
+jobscats.each do |jobcat|
+  JobCategorie.create(
+    name: jobcat
+  )
+end
+##################################################################
 
-puts "Creation  COMPLETE & SUCCEED"
+
+puts "SEED SUCCEED - Enjoy"
