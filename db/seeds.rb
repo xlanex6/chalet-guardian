15fd251997cf581f207cbb8f0f836af3ffeb17a0
Alex <alexduval71@gmail.com>
2016-09-11 08:48:05 +0200

    create fake user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+##############      JOB CATEGORIE   ###############################
+puts "Destoy ALL Ski Resort"
+JobCategorie.destroy_all
+puts "Ski Resort Creation..."
+jobscats = ["Administration", "Bar Staff", "Catering", "Chalet Staff", "Chef","Assistant Chef", "Childcare", "Customer Service", "Driving / Transport", "Facilities / Maintenance", "Head Chef", "Health / Beauty", "Housekeeping", "Instructors / Guides", "Management", "Resort Reps", "Retail" ]
+jobscats.each do |jobcat|
+  JobCategorie.create(
+  name: jobcat
+  )
+end
 ##############  SKI  RESORT  ################
 puts "Destoy Ski Resort"
   SkiResort.destroy_all
@@ -15,6 +25,22 @@ puts "Creation Ski Resort COMPLETE & SUCCEED"
 puts "Destoy ALL User"
   User.destroy_all
 puts "User Creation..."
+  10.times do
+    jobcat = JobCategorie.all.sample
+    User.create(
+      email: Faker::Internet.email,
+      password: "123soleil",
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      phone: Faker::PhoneNumber.phone_number,
+      company: Faker::Company.name,
+      website:"www.#{Faker::Internet.domain_word}.#{Faker::Internet.domain_suffix}",
+      description: Faker::Lorem.sentences(5),
+      available_start_time: Faker::Date.forward(20),
+      available_end_time: Faker::Date.forward(79) + 20,
+      job_categorie: jobcat
+    )
+  end
 
 puts "Creation  COMPLETE & SUCCEED"
 
@@ -33,17 +59,7 @@ puts "Creation..."
 puts "Creation  COMPLETE & SUCCEED"
 
 
-##############      JOB CATEGORIE   ###############################
-puts "Destoy ALL Ski Resort"
-  JobCategorie.destroy_all
-puts "Ski Resort Creation..."
-jobscats = ["Administration", "Bar Staff", "Catering", "Chalet Staff", "Chef","Assistant Chef", "Childcare", "Customer Service", "Driving / Transport", "Facilities / Maintenance", "Head Chef", "Health / Beauty", "Housekeeping", "Instructors / Guides", "Management", "Resort Reps", "Retail" ]
-jobscats.each do |jobcat|
-  JobCategorie.create(
-    name: jobcat
-  )
-end
 ##################################################################
 
 
-puts "SEED SUCCEED - Enjoy"
+puts "SEED SUCCEED - Enjoy ;)"
