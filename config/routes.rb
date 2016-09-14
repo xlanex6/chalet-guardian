18a83c58e05bcb1aa451853c671cff04c627539b
Alex <alexduval71@gmail.com>
2016-09-14 10:49:30 +0200

    container and center in devise new/edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
+  resources 
+
   mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
