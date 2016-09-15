1a98d0734b90e30d2e23cd26cb8f69bbeccb84fb
Alex <alexduval71@gmail.com>
2016-09-15 20:53:18 +0200

    momentjs config DONE

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,16 @@
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require momentjs
 //= require bootstrap-datepicker
 //= require_tree .
 
 
 $(function() {
     $('input.datepicker').data({behaviour: "datepicker"}).datepicker();
+});
+
+
+$('.timestring').each(function() {
+  this.textContent = moment(this.textContent).format('MMMM YYYY');
 });
