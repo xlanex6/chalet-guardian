64193630680fcba623b8483b7abda317dfd0249e
Alex <alexduval71@gmail.com>
2016-10-08 16:16:34 +0200

    user show display with style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:show]
+before_action :set_user, only: [:show, :edit]
 
   def show
+    authorize @user
+  end
+
+  def edit
   end
 
   private
 
 def set_user
   @user = User.find(params[:id])
-  authorize @user
 end
 
 end
