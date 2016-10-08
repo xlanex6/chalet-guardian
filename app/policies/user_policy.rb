ae17f9ac09e241751dc8dd18aa86a74d15e43cbc
Alex <alexduval71@gmail.com>
2016-10-08 16:24:51 +0200

    user policy on user edit and update

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -8,4 +8,18 @@ class UserPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    owner?
+  end
+
+  private
+
+  def owner?
+    record.user == user
+  end
 end
